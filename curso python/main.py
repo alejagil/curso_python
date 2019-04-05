379e78d456aaf6c5b7c9102194ce6e845ff2c58f
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-04-04 20:32:44 -0500

    agregar main al proyecto

---
+++ curso python/main.py
@@ -0,0 +1,36 @@
+""" #print("Hello word")
+
+
+#entrada de datos
+#la potencia es con *, modulo % (es el residuo de la division)
+x=int(input("Ingrese dato: "))
+y=int(input("Ingrese dato: "))
+print (10%2)
+
+a = True
+b = False
+print (a)
+"""  """
+a = 5
+if a<4:
+     print("hola")
+     print("mundo")
+elif a<5:
+     print("es menor que 5")
+    
+else:
+    print("No es menor")
+""" 
+ """
+a=7
+while a==7:
+    print("estoy en while")
+    a = int(input("ingrese el nuevo valor de a: "))
+    if a != 7:
+        break;
+ """
+
+ a=3
+ b=4
+ if a==3 and b==4:
+     print("es correcto")
